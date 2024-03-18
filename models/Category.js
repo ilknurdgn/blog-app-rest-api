416327086a814ea0f252f040dd12d6508c691ad8
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-18 11:44:31 +0300

    feat(models): create Category model

---
+++ models/Category.js
@@ -0,0 +1,13 @@
+const mongooose = require('mongoose');
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Category', CategorySchema);
