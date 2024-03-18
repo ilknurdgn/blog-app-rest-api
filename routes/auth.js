8b82dc1a86fda2ba5b7c82abca56b0f3e6c03720
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-18 21:46:53 +0300

    feat(pages): create login function

--- routes/auth.js
+++ routes/auth.js
@@ -21,4 +21,22 @@ router.post('/register', async (req, res) => {
   }
 });
 
+//LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json(' Wrong credentials!');
+
+    const validate = await bcrypt.compare(req.body.password, user.password);
+    if (!validate) {
+      return res.status(400).json('Wrong credentials!');
+    }
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
