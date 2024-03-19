c3a6b2c3f0cc50d048cd76957d2a9a9a0e537610
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-19 18:02:03 +0300

    feat(routes): create 'delete post' function

--- routes/posts.js
+++ routes/posts.js
@@ -20,14 +20,14 @@ router.put('/:id', async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.username === req.body.username) {
       try {
-        const updatePost = await Post.findByIdAndUpdate(
+        const updatedPost = await Post.findByIdAndUpdate(
           req.params.id,
           {
             $set: req.body,
           },
           { new: true }
         );
-        res.status(200).json(updatePost);
+        res.status(200).json(updatedPost);
       } catch (error) {
         res.status(500).json(error);
       }
@@ -36,6 +36,27 @@ router.put('/:id', async (req, res) => {
     }
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+// DELETE POST
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.username === req.body.username) {
+      try {
+        await post.deleteOne({ _id: req.params.id });
+
+        res.status(200).json('Post has been deleted.');
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json('You can delete only your post!');
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
