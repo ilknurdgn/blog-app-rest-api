a1bcf678321c11cc2acceeb825cd5960b6748305
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-19 17:30:06 +0300

    feat(routes): create 'update post' function

--- routes/posts.js
+++ routes/posts.js
@@ -14,4 +14,29 @@ router.post('/', async (req, res) => {
   }
 });
 
+//UPDATE POST
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatePost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatePost);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json('You can update only post!');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
