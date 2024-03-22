d2599e7b2f13beeab3b1167ef1cd829da825c578
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-22 11:41:13 +0300

    feat(index.js): add file upload feature

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const authRoute = require('./routes/auth');
 const usersRoute = require('./routes/users');
 const postsRoute = require('./routes/posts');
 const categoriesRoute = require('./routes/categories');
+const multer = require('multer');
 
 dotenv.config();
 app.use(express.json());
@@ -16,6 +17,20 @@ mongoose
     console.log('Connected to MongoDB');
   })
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
