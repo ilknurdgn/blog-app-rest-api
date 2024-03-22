f0145453ddd8a371f1713ff6a133ee1fd2db5082
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-22 10:04:13 +0300

    feat(routes): create 'get all categories' function

--- routes/categories.js
+++ routes/categories.js
@@ -12,4 +12,14 @@ router.post('/', async (req, res) => {
   }
 });
 
+//GET ALL CATEGORIES
+router.get('/', async (req, res) => {
+  try {
+    const cats = await Category.find();
+    res.status(200).json(cats);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
