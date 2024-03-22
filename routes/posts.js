6d2f7368182bf101b7571adbc5f11ac200d2b463
ilknur doğan <ilknurdogan254@gmail.com>
2024-03-22 09:49:13 +0300

    feat(rotes): create 'get all posts' function

--- routes/posts.js
+++ routes/posts.js
@@ -70,4 +70,28 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+//GET ALL POST
+router.get('/', async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
